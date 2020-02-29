824338c3e91ae6a47b94576b27b59bb982f7f398
rishabkashyap <rishabkashyap16@gmail.com>
2020-02-29 13:36:06 +0530

    first web page htp method default

---
+++ AddServlet.java
@@ -0,0 +1,16 @@
+//import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+
+public class AddServlet extends HttpServlet{
+	        public void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k = i+j;
+		PrintWriter out = res.getWriter();
+		out.println("result is " + k);
+	}
+	
+} 
